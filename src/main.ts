4e1828f7aa0430124f9f978a75ce5d5a48cdfcbe
kennyhazzar <56494597+kennyhazzar@users.noreply.github.com>
2022-06-10 00:16:29 +0700

    swagger init

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
-  console.log(process.env.MONGO_URI);
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Nested API')
+    .setDescription(
+      'Create an API (Nested) with just one table having categories (id, name, parentId)' +
+        ' and the api response should be having nested children upto n levels.',
+    )
+    .setVersion('1.0')
+    .addTag('nested')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
